60b5d6b0ec1ef4012cb908612cd89b54611ff8f8
24LP6042 24LP6042 <24LP6042@G303-CC0142.local>
2025-02-21 10:34:27 +0800

    up

--- src/components/ProfileInfoStep.jsx
+++ src/components/ProfileInfoStep.jsx
@@ -3,6 +3,8 @@ import { InputFeild } from "./InputFeild"
 
 export const ProfileInfoStep = ({ formValues, setFormValues, nextStep, currentStep, formErrors, setFormErrors, prevStep }) => {
 
+    const imageRef = useRef();
+
     const [url, setUrl] = useState('');
 
     const onChange = (event) => {
@@ -14,9 +16,12 @@ export const ProfileInfoStep = ({ formValues, setFormValues, nextStep, currentSt
     //     setFormErrors((prev) => ({ ...prev, [event.target.name]: '' }))
     // }
 
+    const uploadImage = () => {
+        
+    }
+
     const handleNext = (event) => {
         event.preventDefault();
-
 
         // nextStep();
     }
@@ -26,7 +31,13 @@ export const ProfileInfoStep = ({ formValues, setFormValues, nextStep, currentSt
 
             <InputFeild type="date" required label='Date' name="date" onChange={onChange} error={formErrors['date']} />
 
-            {url ? <img className="h-40 w-full object-cover" src={url} /> : <InputFeild type="file" required label='Profile image' name="profileImage" onChange={onChange} error={formErrors['profileImage']} />}
+            {url && <img className="h-40 w-full object-cover" src={url} />}
+
+            <div onClick={uploadImage}>Add image</div>
+
+            <input className="hidden" type="file" ref={imageRef} />
+
+            {/* <InputFeild type="file" required label='Profile image' name="profileImage" onChange={onChange} error={formErrors['profileImage']} /> */}
 
         </form>
     )
